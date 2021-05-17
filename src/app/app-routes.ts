8dcd5e647598f4848e6de7d57ec27b39c39e051d
dev-jv <dev.javierleon@gmail.com>
2021-05-17 12:02:32 -0500

    nine

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -2,9 +2,22 @@ import {Routes, RouterModule} from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import {UserComponent} from './components/user/user.component';
 
+import {NewUserComponent} from './components/user/new-user.component';
+import {EditUserComponent} from './components/user/edit-user.component';
+import {UserDetailsComponent} from './components/user/user-details.component';
+
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'user', component: UserComponent },
+  {
+    path: 'user',
+    component: UserComponent,
+    children: [
+      { path: 'new', component: NewUserComponent },
+      { path: 'edit', component: EditUserComponent },
+      { path: 'details', component: UserDetailsComponent },
+      { path: '**', pathMatch: 'full', redirectTo: 'new' }
+    ]
+  },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
