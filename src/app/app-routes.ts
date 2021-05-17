409f5cfcc6e93b9f553e77fb9cdd9824bd9e9a77
dev-jv <dev.javierleon@gmail.com>
2021-05-17 12:26:19 -0500

    ten

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -2,21 +2,24 @@ import {Routes, RouterModule} from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import {UserComponent} from './components/user/user.component';
 
-import {NewUserComponent} from './components/user/new-user.component';
-import {EditUserComponent} from './components/user/edit-user.component';
-import {UserDetailsComponent} from './components/user/user-details.component';
+// import {NewUserComponent} from './components/user/new-user.component';
+// import {EditUserComponent} from './components/user/edit-user.component';
+// import {UserDetailsComponent} from './components/user/user-details.component';
+
+import {USER_ROUTES} from './components/user/user.routes';
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
   {
     path: 'user',
     component: UserComponent,
-    children: [
-      { path: 'new', component: NewUserComponent },
-      { path: 'edit', component: EditUserComponent },
-      { path: 'details', component: UserDetailsComponent },
-      { path: '**', pathMatch: 'full', redirectTo: 'new' }
-    ]
+    children: USER_ROUTES
+    // children: [
+      // { path: 'new', component: NewUserComponent },
+      // { path: 'edit', component: EditUserComponent },
+      // { path: 'details', component: UserDetailsComponent },
+      // { path: '**', pathMatch: 'full', redirectTo: 'new' }
+    // ]
   },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
---
+++ src/app/components/user/user.routes.ts
@@ -0,0 +1,12 @@
+import {Route, Routes} from '@angular/router';
+
+import {NewUserComponent} from './new-user.component';
+import {EditUserComponent} from './edit-user.component';
+import {UserDetailsComponent} from './user-details.component';
+
+export const USER_ROUTES: Routes = [
+  { path: 'new', component: NewUserComponent },
+  { path: 'edit', component: EditUserComponent },
+  { path: 'details', component: UserDetailsComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'new' }
+];
